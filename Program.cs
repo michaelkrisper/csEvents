de7dd0bf27e1db8c07040c1fc22c7f4d5048556b
Michael Krisper <michael.krisper@gmail.com>
2016-08-07 13:43:30 +0200

    implemented observer pattern

--- Program.cs
+++ Program.cs
@@ -1,24 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace ConsoleApplication
+namespace csEvents
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            var c = new Client();
+            var c = new EventSubject();
             c.Finished += () => Console.WriteLine("Finished.");
             c.DoWork();
+
+            var s = new ConcreteSubject();
+            s.Attach(new ConcreteObserver());
+            s.DoWork();
+
+            Console.ReadKey();
         }
     }
 
-    public class Client
+    public class EventSubject
     {
         public event Action Finished;
+
         public void DoWork()
         {
             Console.WriteLine("Doing work.");
             Finished?.Invoke();
         }
+    }
+
+    public class Subject
+    {
+        private readonly HashSet<IObserver> _observers = new HashSet<IObserver>();
+        public void Attach(IObserver observer) => _observers.Add(observer);
+        public void Detach(IObserver observer) => _observers.Remove(observer);
+        protected void Notify() => _observers.ToList().ForEach(o => o.Update(this));
+    }
+
+    public class ConcreteSubject : Subject
+    {
+        public void DoWork()
+        {
+            Console.WriteLine("Doing work.");
+            Notify();
+        }
+    }
+
+    public interface IObserver
+    {
+        void Update(object sender);
+    }
+
+    public class ConcreteObserver : IObserver
+    {
+        public void Update(object sender) => Console.WriteLine("Finished.");
     }
 }
